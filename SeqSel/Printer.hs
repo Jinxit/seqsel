25f472c3bbbbea08d6f3f0f6feb89d31f4bcf521
Lucas Åström <lucasas@kth.se>
2015-10-22 15:59:13 +0200

    output formatting

--- SeqSel/Printer.hs
+++ SeqSel/Printer.hs
@@ -1,19 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
-module SeqSel.Printer (printExpr) where
+module SeqSel.Printer (printExpr, printVar, printRoot) where
 
-import Control.Monad.Writer
+import Data.Monoid
 import qualified Data.Text.Lazy as L
 import SeqSel.Parser
 
-indent :: Int -> String
-indent = flip replicate ' '
+indent :: Int -> L.Text
+indent n = L.pack (replicate n ' ')
 
-printExpr :: Expr -> L.Text
-printExpr (Selector name children) = "/* " <> L.pack name <> " */ (" <> children' <> ")"
+printExpr :: Int -> Expr -> L.Text
+printExpr n (Selector node children) = "\n" <> indent n <> "/* " <> L.pack node
+                                    <> " */\n" <> indent n <> "(" <> children' <> "\n" <> indent n <> ")"
   where
-    children' = L.intercalate " || " (map printExpr children)
-printExpr (Sequence name children) = "/* " <> L.pack name <> " */ (" <> children' <> ")"
+    children' = L.intercalate ("\n" <> indent n <> "|| ")
+                              (map (printExpr (n + 4)) children)
+printExpr n (Sequence node children) = "\n" <> indent n <> "/* " <> L.pack node
+                                    <> " */\n" <> indent n <> "(" <> children' <> ")"
   where
-    children' = L.intercalate " && " (map printExpr children)
-printExpr (Condition cond) = L.pack cond
-printExpr (Call fun) = L.pack fun
+    children' = L.intercalate " && " (map (printExpr (n + 4)) children)
+printExpr _ (Condition cond) = L.pack cond
+printExpr _ (Call fun) = L.pack fun
+
+printVar :: Var -> L.Text
+printVar (Var str) = L.pack str <> ";\n"
+
+printRoot :: Expr -> L.Text
+printRoot expr = "bool " <> L.pack (name expr) <> "()\n{\n"
+              <> indent 4 <> "return" <> printExpr 12 expr <> ";\n}"
